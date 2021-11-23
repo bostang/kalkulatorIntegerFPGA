fb320ea11876d85d24b918f62b881dbc85529668
Bostang Palaguna <64513224+bostang@users.noreply.github.com>
2021-11-23 22:00:19 +0700

    menambahkan blok output display
    blok output display berfungsi untuk menampilkan hasil perhitungan yang kita peroleh sebelumnya pada 7-segment

--- output.vhd
+++ output.vhd
@@ -1,6 +1,6 @@
--- modul output
-	-- Blok yang mengatur tampilan dari bcd 7 segmen
-	-- berdasarkan sinyal-sinyal input dari fsm
+-- modul output generator
+	-- Blok yang menampilkan hasil perhitungan yang telah dipisah menggunakan modulo
+	-- pada  7-segment
 
 -- libary
 library ieee;
@@ -13,139 +13,147 @@ entity output is
 	port
 	(
 		reset							   : in std_logic;
-		hasil1,hasil2, s_input, xi, yi	   : in std_logic_vector(3 downto 0); -- input angka, dari register 2
-		out_on, outx1, outx2, outy1, outy2 : in std_logic; 
-		a_7s, b_7s						   : out std_logic_vector(6 downto 0) -- output hasil operasi
+		hasil1,hasil2, hasil3, hasil4	   : in std_logic_vector(3 downto 0); -- input angka, dari register 2
+		out_on							   : in std_logic; -- menyalakan output
+		digitSS						       : out std_logic_vector(3 downto 0) -- digit satuan/puluhan/ratusan/ribuan pada 7-segment
+		sevenSegment 					   : out std_logic_vector(7 downto 0) -- segmen yang menyala pada 7-segment
+		
 	); 
 end output;
 
 -- arsitektur
-architecture display7S_arc of output is
+architecture arc_output of output is
+	signal selector: std_logic_vector(3 downto 0):= "1111"; -- sinyal internal untuk ring counter
+	signal dig3, dig2, dig1, dig0 : std_logic_vector(6 downto 0); -- angka yang ditampilkan pada masing-masing digit
+	signal clk_divider : unsigned(15 downto 0):=(others=>'0'); -- di-inisiasi dengan nol
+	signal clk_out : std_logic;
+
 begin
-	process(out_on, hasil1, hasil2, s_input, xi, yi, outx1, outx2, outy1, outy2) 
+
+		-- mengonversi dari bit ke 7-segment
+	case hasil1 is
+		  when "0000" =>
+			 dig0 <= "0000001"; -- 0
+		  when "0001" =>
+			 dig0 <= "1001111"; -- 1
+		  when "0010" =>
+			 dig0 <= "0010010"; -- 2
+		  when "0011" =>
+			 dig0 <= "0000110"; -- 3 
+		  when "0100" =>
+			 dig0 <= "1001100"; -- 4    
+		  when "0101" =>
+			 dig0 <= "0100100"; -- 5
+		  when "0110" =>
+			 dig0 <= "0100000"; -- 6
+		  when "0111" =>
+			 dig0 <= "0001111"; -- 7
+		  when "1000" =>
+			 dig0 <= "0000000"; -- 8
+		  when "1001" =>
+			 dig0 <= "0000100"; -- 9
+
+	case hasil2 is
+	  when "0000" =>
+		 dig1 <= "0000001"; -- 0
+	  when "0001" =>
+		 dig1 <= "1001111"; -- 1
+	  when "0010" =>
+		 dig1 <= "0010010"; -- 2
+	  when "0011" =>
+		 dig1 <= "0000110"; -- 3 
+	  when "0100" =>
+		 dig1 <= "1001100"; -- 4    
+	  when "0101" =>
+		 dig1 <= "0100100"; -- 5
+	  when "0110" =>
+		 dig1 <= "0100000"; -- 6
+	  when "0111" =>
+		 dig1 <= "0001111"; -- 7
+	  when "1000" =>
+		 dig1 <= "0000000"; -- 8
+	  when "1001" =>
+		 dig1 <= "0000100"; -- 9
+
+	case hasil3 is
+	  when "0000" =>
+		 dig2 <= "0000001"; -- 0
+	  when "0001" =>
+		 dig2 <= "1001111"; -- 1
+	  when "0010" =>
+		 dig2 <= "0010010"; -- 2
+	  when "0011" =>
+		 dig2 <= "0000110"; -- 3 
+	  when "0100" =>
+		 dig2 <= "1001100"; -- 4    
+	  when "0101" =>
+		 dig2 <= "0100100"; -- 5
+	  when "0110" =>
+		 dig2 <= "0100000"; -- 6
+	  when "0111" =>
+		 dig2 <= "0001111"; -- 7
+	  when "1000" =>
+		 dig2 <= "0000000"; -- 8
+	  when "1001" =>
+		 dig2 <= "0000100"; -- 9
+
+	case hasil4 is
+		  when "0000" =>
+			 dig3 <= "0000001"; -- 0
+		  when "0001" =>
+			 dig3 <= "1001111"; -- 1
+		  when "0010" =>
+			 dig3 <= "0010010"; -- 2
+		  when "0011" =>
+			 dig3 <= "0000110"; -- 3 
+		  when "0100" =>
+			 dig3 <= "1001100"; -- 4    
+		  when "0101" =>
+			 dig3 <= "0100100"; -- 5
+		  when "0110" =>
+			 dig3 <= "0100000"; -- 6
+		  when "0111" =>
+			 dig3 <= "0001111"; -- 7
+		  when "1000" =>
+			 dig3 <= "0000000"; -- 8
+		  when "1001" =>
+			 dig3 <= "0000100"; -- 9
+			 
+	clockDivider : process(reset,clk)
+		-- mendapatkan clock dengan frekuensi yang kita inginkan
+		begin
+		  if(reset='0') then -- reset = '0' berarti tombol reset ditekan
+			clk_divider   <= (others=>'0');
+		  elsif(rising_edge(clk)) then
+			clk_divider   <= clk_divider + 1;
+		  end if;
+	end process clockDivider;
+
+	clk_out <= clk_divider(15);  -- f_out = 50 Mhz /(2^15)
+
+	gilir : process(clk_out,reset)
+		-- membuat sekuens 1110 -> 1101 -> 1011 -> 0111 dengan ring counter
+    begin
+	    if reset = '0' then
+	        selector <= "1110";
+	    elsif rising_edge(clk_out) then -- sama dengan (clk_out'event and clk_out = 1)
+	        selector(1) <= selector(0);
+	        selector(2) <= selector(1);
+	        selector(3) <= selector(2);
+	        selector(0) <= selector(3);
+	    end if;
+    end process gilir;
+
+	seleksi : process(selector)
+		-- melakukan proses penggiliran 7-segment dengan sekuens dari ring counter
 	begin
-		if reset = '1' then
-			a_7s 	<= "1111111";
-			b_7s	<= "1111111";
-		elsif reset = '0' and out_on ='1' then
-			case hasil2 is -- display hasil operasi 
-				when "0000"=> a_7S <="0000001"; -- '0'
-				when "0001"=> a_7S <="1001111"; -- '1'
-				when "0010"=> a_7S <="0010010"; -- '2'
-				when "0011"=> a_7S <="0000110"; -- '3' 
-				when "0100"=> a_7S <="1001100"; -- '4'
-				when "0101"=> a_7S <="0100100"; -- '5' 
-				when "0110"=> a_7S <="0100000"; -- '6' 
-				when "0111"=> a_7S <="0001111"; -- '7' 
-				when "1000"=> a_7S <="0000000"; -- '8' 
-				when "1001"=> a_7S <="0000100"; -- '9' 
-				when others=> a_7S <="1111111";  
-			end case;
-			case hasil1 is -- display hasil operasi 
-				when "0000"=> b_7S <="0000001"; -- '0'
-				when "0001"=> b_7S <="1001111"; -- '1'
-				when "0010"=> b_7S <="0010010"; -- '2'
-				when "0011"=> b_7S <="0000110"; -- '3' 
-				when "0100"=> b_7S <="1001100"; -- '4'
-				when "0101"=> b_7S <="0100100"; -- '5' 
-				when "0110"=> b_7S <="0100000"; -- '6' 
-				when "0111"=> b_7S <="0001111"; -- '7' 
-				when "1000"=> b_7S <="0000000"; -- '8' 
-				when "1001"=> b_7S <="0000100"; -- '9' 
-				when others=> b_7S <="1111111";  
-			end case;
-		elsif reset ='0' and out_on = '0' then
-			if (outx1 = '1' and outx2 = '0' and outy1 ='0' and outy2= '0') then
-				case s_input is -- display hasil operasi 
-					when "0000"=> b_7S <="0000001"; -- '0'
-					when "0001"=> b_7S <="1001111"; -- '1'
-					when "0010"=> b_7S <="0010010"; -- '2'
-					when "0011"=> b_7S <="0000110"; -- '3' 
-					when "0100"=> b_7S <="1001100"; -- '4'
-					when "0101"=> b_7S <="0100100"; -- '5' 
-					when "0110"=> b_7S <="0100000"; -- '6' 
-					when "0111"=> b_7S <="0001111"; -- '7' 
-					when "1000"=> b_7S <="0000000"; -- '8' 
-					when "1001"=> b_7S <="0000100"; -- '9' 
-					when others=> b_7S <="1111111";  
-				end case;
-				a_7s	<= "1111111";
-			elsif outx1 = '1' and outx2 = '1' and outy1 ='0' and outy2= '0' then
-				case xi is -- display hasil operasi 
-					when "0000"=> a_7S <="0000001"; -- '0'
-					when "0001"=> a_7S <="1001111"; -- '1'
-					when "0010"=> a_7S <="0010010"; -- '2'
-					when "0011"=> a_7S <="0000110"; -- '3' 
-					when "0100"=> a_7S <="1001100"; -- '4'
-					when "0101"=> a_7S <="0100100"; -- '5' 
-					when "0110"=> a_7S <="0100000"; -- '6' 
-					when "0111"=> a_7S <="0001111"; -- '7' 
-					when "1000"=> a_7S <="0000000"; -- '8' 
-					when "1001"=> a_7S <="0000100"; -- '9' 
-					when others=> a_7S <="1111111";  
-				end case;
-				case s_input is -- display hasil operasi 
-					when "0000"=> b_7S <="0000001"; -- '0'
-					when "0001"=> b_7S <="1001111"; -- '1'
-					when "0010"=> b_7S <="0010010"; -- '2'
-					when "0011"=> b_7S <="0000110"; -- '3' 
-					when "0100"=> b_7S <="1001100"; -- '4'
-					when "0101"=> b_7S <="0100100"; -- '5' 
-					when "0110"=> b_7S <="0100000"; -- '6' 
-					when "0111"=> b_7S <="0001111"; -- '7' 
-					when "1000"=> b_7S <="0000000"; -- '8' 
-					when "1001"=> b_7S <="0000100"; -- '9' 
-					when others=> b_7S <="1111111";  
-				end case;
-			elsif outx1 = '0' and outx2 = '0' and outy1 ='1' and outy2= '0' then
-				case s_input is -- display hasil operasi 
-					when "0000"=> b_7S <="0000001"; -- '0'
-					when "0001"=> b_7S <="1001111"; -- '1'
-					when "0010"=> b_7S <="0010010"; -- '2'
-					when "0011"=> b_7S <="0000110"; -- '3' 
-					when "0100"=> b_7S <="1001100"; -- '4'
-					when "0101"=> b_7S <="0100100"; -- '5' 
-					when "0110"=> b_7S <="0100000"; -- '6' 
-					when "0111"=> b_7S <="0001111"; -- '7' 
-					when "1000"=> b_7S <="0000000"; -- '8' 
-					when "1001"=> b_7S <="0000100"; -- '9' 
-					when others=> b_7S <="1111111";  
-				end case;
-				a_7s	<= "1111111";
-			elsif outx1 = '0' and outx2 = '0' and outy1 ='1' and outy2= '1' then
-				case yi is -- display hasil operasi 
-					when "0000"=> a_7S <="0000001"; -- '0'
-					when "0001"=> a_7S <="1001111"; -- '1'
-					when "0010"=> a_7S <="0010010"; -- '2'
-					when "0011"=> a_7S <="0000110"; -- '3' 
-					when "0100"=> a_7S <="1001100"; -- '4'
-					when "0101"=> a_7S <="0100100"; -- '5' 
-					when "0110"=> a_7S <="0100000"; -- '6' 
-					when "0111"=> a_7S <="0001111"; -- '7' 
-					when "1000"=> a_7S <="0000000"; -- '8' 
-					when "1001"=> a_7S <="0000100"; -- '9' 
-					when others=> a_7S <="1111111";  
-				end case;
-				case s_input is -- display hasil operasi 
-					when "0000"=> b_7S <="0000001"; -- '0'
-					when "0001"=> b_7S <="1001111"; -- '1'
-					when "0010"=> b_7S <="0010010"; -- '2'
-					when "0011"=> b_7S <="0000110"; -- '3' 
-					when "0100"=> b_7S <="1001100"; -- '4'
-					when "0101"=> b_7S <="0100100"; -- '5' 
-					when "0110"=> b_7S <="0100000"; -- '6' 
-					when "0111"=> b_7S <="0001111"; -- '7' 
-					when "1000"=> b_7S <="0000000"; -- '8' 
-					when "1001"=> b_7S <="0000100"; -- '9' 
-					when others=> b_7S <="1111111";  
-				end case;
-			else 
-				a_7s 	<= "1111111";
-				b_7s	<= "1111111";
-			end if;
-		else
-			a_7s 	<= "1111111";
-			b_7s	<= "1111111";
-		end if;
-	end process;
-end display7S_arc;
+		digOut <= selector;
+		case selector is
+			when "0111" => segOut <= dig3; -- 0 (most significant byte [MSB])
+			when "1011" => segOut <= dig2; -- 0
+			when "1101" => segOut <= dig1; -- 5
+			when "1110" => segOut <= dig0; -- 5 (least significant byte [LSB])
+			when others => segOut <= "1111111";
+		end case;
+	end process seleksi;
+end arc_output;
