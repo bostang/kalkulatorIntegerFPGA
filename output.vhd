e0510ead336f6fc856f91af12a96a2c733d54a9b
Bostang Palaguna <64513224+bostang@users.noreply.github.com>
2021-11-23 23:08:43 +0700

    mengubah blok output
    blok output : blok untuk menampilkan hasil perhitungan yang telah dipisah dari modulo pada 7-segment

--- output.vhd
+++ output.vhd
@@ -12,11 +12,12 @@ use ieee.std_logic_unsigned.all;
 entity output is
 	port
 	(
+		clk 							   : in std_logic;
 		reset							   : in std_logic;
 		hasil1,hasil2, hasil3, hasil4	   : in std_logic_vector(3 downto 0); -- input angka, dari register 2
 		out_on							   : in std_logic; -- menyalakan output
-		digitSS						       : out std_logic_vector(3 downto 0) -- digit satuan/puluhan/ratusan/ribuan pada 7-segment
-		sevenSegment 					   : out std_logic_vector(7 downto 0) -- segmen yang menyala pada 7-segment
+		digOut						   : out std_logic_vector(3 downto 0); -- digit satuan/puluhan/ratusan/ribuan pada 7-segment
+		segOut	 					       : out std_logic_vector(7 downto 0) -- segmen yang menyala pada 7-segment
 		
 	); 
 end output;
@@ -24,101 +25,115 @@ end output;
 -- arsitektur
 architecture arc_output of output is
 	signal selector: std_logic_vector(3 downto 0):= "1111"; -- sinyal internal untuk ring counter
-	signal dig3, dig2, dig1, dig0 : std_logic_vector(6 downto 0); -- angka yang ditampilkan pada masing-masing digit
+	signal dig3, dig2, dig1, dig0 : std_logic_vector(7 downto 0); -- angka yang ditampilkan pada masing-masing digit
 	signal clk_divider : unsigned(15 downto 0):=(others=>'0'); -- di-inisiasi dengan nol
 	signal clk_out : std_logic;
 
 begin
 
 		-- mengonversi dari bit ke 7-segment
-	case hasil1 is
-		  when "0000" =>
-			 dig0 <= "0000001"; -- 0
-		  when "0001" =>
-			 dig0 <= "1001111"; -- 1
-		  when "0010" =>
-			 dig0 <= "0010010"; -- 2
-		  when "0011" =>
-			 dig0 <= "0000110"; -- 3 
-		  when "0100" =>
-			 dig0 <= "1001100"; -- 4    
-		  when "0101" =>
-			 dig0 <= "0100100"; -- 5
-		  when "0110" =>
-			 dig0 <= "0100000"; -- 6
-		  when "0111" =>
-			 dig0 <= "0001111"; -- 7
-		  when "1000" =>
-			 dig0 <= "0000000"; -- 8
-		  when "1001" =>
-			 dig0 <= "0000100"; -- 9
+	process(reset, hasil1, hasil2, hasil3, hasil4, out_on)
+	begin
+		case hasil1 is
+			when "0000" =>
+				dig0 <= "00000011"; -- 0
+		 	when "0001" =>
+				dig0 <= "10011111"; -- 1
+		 	when "0010" =>
+				dig0 <= "00100101"; -- 2
+		 	when "0011" =>
+				dig0 <= "00001101"; -- 3 
+		 	when "0100" =>
+				dig0 <= "10011001"; -- 4    
+			when "0101" =>
+				dig0 <= "01001001"; -- 5
+			when "0110" =>
+				dig0 <= "01000001"; -- 6
+	 		when "0111" =>
+				dig0 <= "00011111"; -- 7
+			when "1000" =>
+				dig0 <= "00000001"; -- 8
+			when "1001" =>
+				dig0 <= "00001001"; -- 9
+			when others =>
+				dig0 <= "11111111";
+			end case;
 
-	case hasil2 is
-	  when "0000" =>
-		 dig1 <= "0000001"; -- 0
-	  when "0001" =>
-		 dig1 <= "1001111"; -- 1
-	  when "0010" =>
-		 dig1 <= "0010010"; -- 2
-	  when "0011" =>
-		 dig1 <= "0000110"; -- 3 
-	  when "0100" =>
-		 dig1 <= "1001100"; -- 4    
-	  when "0101" =>
-		 dig1 <= "0100100"; -- 5
-	  when "0110" =>
-		 dig1 <= "0100000"; -- 6
-	  when "0111" =>
-		 dig1 <= "0001111"; -- 7
-	  when "1000" =>
-		 dig1 <= "0000000"; -- 8
-	  when "1001" =>
-		 dig1 <= "0000100"; -- 9
+		case hasil2 is
+			when "0000" =>
+				dig1 <= "00000011"; -- 0
+		 	when "0001" =>
+				dig1 <= "10011111"; -- 1
+		 	when "0010" =>
+				dig1 <= "00100101"; -- 2
+		 	when "0011" =>
+				dig1 <= "00001101"; -- 3 
+		 	when "0100" =>
+				dig1 <= "10011001"; -- 4    
+			when "0101" =>
+				dig1 <= "01001001"; -- 5
+			when "0110" =>
+				dig1 <= "01000001"; -- 6
+	 		when "0111" =>
+				dig1 <= "00011111"; -- 7
+			when "1000" =>
+				dig1 <= "00000001"; -- 8
+			when "1001" =>
+				dig1 <= "00001001"; -- 9
+			when others =>
+				dig1 <= "11111111";
+			end case;
 
-	case hasil3 is
-	  when "0000" =>
-		 dig2 <= "0000001"; -- 0
-	  when "0001" =>
-		 dig2 <= "1001111"; -- 1
-	  when "0010" =>
-		 dig2 <= "0010010"; -- 2
-	  when "0011" =>
-		 dig2 <= "0000110"; -- 3 
-	  when "0100" =>
-		 dig2 <= "1001100"; -- 4    
-	  when "0101" =>
-		 dig2 <= "0100100"; -- 5
-	  when "0110" =>
-		 dig2 <= "0100000"; -- 6
-	  when "0111" =>
-		 dig2 <= "0001111"; -- 7
-	  when "1000" =>
-		 dig2 <= "0000000"; -- 8
-	  when "1001" =>
-		 dig2 <= "0000100"; -- 9
+		case hasil3 is
+			when "0000" =>
+				dig2 <= "00000011"; -- 0
+		 	when "0001" =>
+				dig2 <= "10011111"; -- 1
+		 	when "0010" =>
+				dig2 <= "00100101"; -- 2
+		 	when "0011" =>
+				dig2 <= "00001101"; -- 3 
+		 	when "0100" =>
+				dig2 <= "10011001"; -- 4    
+			when "0101" =>
+				dig2 <= "01001001"; -- 5
+			when "0110" =>
+				dig2 <= "01000001"; -- 6
+	 		when "0111" =>
+				dig2 <= "00011111"; -- 7
+			when "1000" =>
+				dig2 <= "00000001"; -- 8
+			when "1001" =>
+				dig2 <= "00001001"; -- 9
+			when others =>
+				dig2 <= "11111111";
+			end case;
 
-	case hasil4 is
-		  when "0000" =>
-			 dig3 <= "0000001"; -- 0
-		  when "0001" =>
-			 dig3 <= "1001111"; -- 1
-		  when "0010" =>
-			 dig3 <= "0010010"; -- 2
-		  when "0011" =>
-			 dig3 <= "0000110"; -- 3 
-		  when "0100" =>
-			 dig3 <= "1001100"; -- 4    
-		  when "0101" =>
-			 dig3 <= "0100100"; -- 5
-		  when "0110" =>
-			 dig3 <= "0100000"; -- 6
-		  when "0111" =>
-			 dig3 <= "0001111"; -- 7
-		  when "1000" =>
-			 dig3 <= "0000000"; -- 8
-		  when "1001" =>
-			 dig3 <= "0000100"; -- 9
-			 
+		case hasil4 is
+			when "0000" =>
+				dig3 <= "00000011"; -- 0
+		 	when "0001" =>
+				dig3 <= "10011111"; -- 1
+		 	when "0010" =>
+				dig3 <= "00100101"; -- 2
+		 	when "0011" =>
+				dig3 <= "00001101"; -- 3 
+		 	when "0100" =>
+				dig3 <= "10011001"; -- 4    
+			when "0101" =>
+				dig3 <= "01001001"; -- 5
+			when "0110" =>
+				dig3 <= "01000001"; -- 6
+	 		when "0111" =>
+				dig3 <= "00011111"; -- 7
+			when "1000" =>
+				dig3 <= "00000001"; -- 8
+			when "1001" =>
+				dig3 <= "00001001"; -- 9
+			when others =>
+				dig3 <= "11111111";
+			end case;			
+	end process;		 
 	clockDivider : process(reset,clk)
 		-- mendapatkan clock dengan frekuensi yang kita inginkan
 		begin
@@ -144,16 +159,20 @@ begin
 	    end if;
     end process gilir;
 
-	seleksi : process(selector)
+	seleksi : process(selector,out_on)
 		-- melakukan proses penggiliran 7-segment dengan sekuens dari ring counter
 	begin
 		digOut <= selector;
-		case selector is
-			when "0111" => segOut <= dig3; -- 0 (most significant byte [MSB])
-			when "1011" => segOut <= dig2; -- 0
-			when "1101" => segOut <= dig1; -- 5
-			when "1110" => segOut <= dig0; -- 5 (least significant byte [LSB])
-			when others => segOut <= "1111111";
-		end case;
+		if (out_on = '0') then
+			segOut <= "11111111";
+		else
+			case selector is
+				when "0111" => segOut <= dig3; -- 0 (most significant byte [MSB])
+				when "1011" => segOut <= dig2; -- 0
+				when "1101" => segOut <= dig1; -- 5
+				when "1110" => segOut <= dig0; -- 5 (least significant byte [LSB])
+				when others => segOut <= "11111111";
+			end case;
+		end if;
 	end process seleksi;
 end arc_output;
