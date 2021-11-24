233b9fe8b297e97a5d098401e48d17beffed2bcc
Bostang Palaguna <64513224+bostang@users.noreply.github.com>
2021-11-24 09:48:56 +0700

    mengubah rangkaian pembagi

--- divider.vhd
+++ divider.vhd
@@ -1,39 +1,24 @@
-LIBRARY IEEE; 
-USE IEEE.STD_LOGIC_1164.ALL; 
-USE IEEE.STD_LOGIC_ARITH.ALL; 
-USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+-- modul divider
+    -- Blok yang menghitung hasil pembagian dari dua input.
+    -- Blok ini menerima input dua buah  bit logic vector panjang 12
+    -- dan mengembalikan hasil operasi berupa bit logic vector panjang 16
 
+-- library
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+-- entitas
 entity divider is
-	port(
-		clk, run: in std_logic;
-		x		: in std_logic_vector (7 downto 0);
-		y		: in std_logic_vector (4 downto 0);
-		output	: out std_logic_vector (7 downto 0);
-		sisa	: out std_logic_vector (3 downto 0));
+    port
+    (
+        x,y    : in std_logic_vector(11 downto 0);
+        result : out std_logic_vector(15 downto 0)
+    );
 end divider;
 
-architecture divider_arc of divider is
-	SIGNAL tempx	: std_logic_vector (6 downto 0);
-	SIGNAL tempy	: std_logic_vector (3 downto 0);
-	SIGNAL hasil	: std_logic_vector (7 downto 0) := "00000000";
-
+-- arsitektur
+architecture arc_divider of divider is
 begin
-	output <= (x(7) XOR y(4)) & hasil(6 downto 0);
-	
-	process (clk, run)
-	begin
-		if (run='0') then
-			tempx <= x(6 downto 0);
-			tempy <= y(3 downto 0);
-			hasil <= "00000000";
-		elsif (clk'EVENT) and (clk='1') then
-			if (tempy(3 downto 0) = "0000") then
-				hasil <= "00000000";
-			elsif (unsigned(tempx(6 downto 0)) >= unsigned(tempy(3 downto 0))) then
-				tempx <= unsigned(tempx(6 downto 0)) - unsigned(tempy(3 downto 0));
-				hasil <= hasil + '1';
-			end if;
-		end if;
-		sisa <= tempx(3 downto 0);
-	end process;
-end divider_arc;
+    result <= std_logic_vector(to_unsigned(to_integer(unsigned(x) / unsigned(y)),16));
+end arc_divider;
